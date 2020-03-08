69762c166a8656ded1b541db4e6746d933a9bb58
mokrijyurij <mokrijyurij@outlook.com>
2020-03-08 19:00:40 +0300

    refactor first app

--- www/mig-agency.loc/admin/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$htmlfiles = glob("../*.php");
-$response = [];
-foreach ($htmlfiles as $file) {
-    array_push($response, basename($file));
-}
-echo json_encode($response);
--- www/mig-agency.loc/app/src/main.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Editor from "./components/editor";
-
-ReactDom.render(<Editor/>, document.getElementById('root'));
